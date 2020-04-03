ccc2f07d895fafa3c344cce84d9410e7fd83ef75
Federico Perez <fedelll@gmail.com>
2020-04-03 21:38:55 +0200

    fixed Hello world

--- Assets/Scripts/test.cs
+++ Assets/Scripts/test.cs
@@ -7,7 +7,7 @@ public class test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Console.WriteLine("Hello World!");
+        Debug.Log("Hello World!");
     }
 
     // Update is called once per frame
