0d2fb4fe7b0b051a78c5d10d7bb0a0a6227a2904
muoz <antmunozga@gmail.com>
2020-04-04 00:59:27 +0200

    Auto stash before merge of "master" and "origin/master"

---
+++ Assets/Scripts/Comida.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Comida : MonoBehaviour
+{
+    // For Alex please code the food shit
+    public float food = 100.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Time.deltaTime
+        food -= 0.1f;
+    }
+}
