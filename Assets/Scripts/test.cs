a6006c39f3d88ed13255f7139ff57a1b7432573f
Federico Perez <fedelll@gmail.com>
2020-04-03 21:34:09 +0200

    Hello world

---
+++ Assets/Scripts/test.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Console.WriteLine("Hello World!");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
