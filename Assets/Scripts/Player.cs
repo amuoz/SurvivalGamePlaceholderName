7a424ac3b497a87e4d4f549645d405559f5a82fb
Federico Perez <fedelll@gmail.com>
2020-04-04 02:44:15 +0200

    new character movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed = 1.5f;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -15,9 +14,50 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        float moveX = 0;
+        float moveY = 0;
+        bool isRightInput = Input.GetKey("d");
+        bool isLeftInput = Input.GetKey("a");
+        bool isDownButton = Input.GetKey("s");
+        bool isUpButton = Input.GetKey("w");
+
+        if (isRightInput) {
+            moveX = 1;
+        } else if (isLeftInput) {
+            moveX = -1;
+        }
+
+        if (isUpButton) {
+            moveY = 1;
+        } else if (isDownButton) {
+            moveY = -1;
+        }
+
+        if (moveX != 0 && moveY != 0) {
+            moveX /= Mathf.Sqrt(2);
+            moveY /= Mathf.Sqrt(2);
+        }
+
+        // if (inputX > 0) {
+        //     moveX = 1;
+        // } else if (inputX < 0) {
+        //     moveX = -1;
+        // }
+
+        // if (inputY > 0) {
+        //     moveY = 1;
+        // } else if (inputY < 0) {
+        //     moveY = -1;
+        // }
+
         GetComponent<Rigidbody2D>().velocity = new Vector2(
-            Input.GetAxis("Horizontal") * speed,
-            Input.GetAxis("Vertical") * speed
+            moveX * speed,
+            moveY * speed
         );
     }
 }
