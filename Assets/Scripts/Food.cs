031d28bb603e54fbcaf4c956a9662c53353ad647
alejandro.martinez <alejandroiddev@gmail.com>
2020-04-04 02:54:05 +0200

    Added food behaviour

---
+++ Assets/Scripts/Food.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Food : MonoBehaviour
+{
+    public float size;
+    
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void OnTriggerEnter2D(Collider2D player)
+    {
+        if(player.name == "Player")
+        {
+            Stomach belly = player.gameObject.GetComponent<Stomach>();
+            belly.Feed(size);
+        }
+        Destroy(this.gameObject);
+    }
+}
