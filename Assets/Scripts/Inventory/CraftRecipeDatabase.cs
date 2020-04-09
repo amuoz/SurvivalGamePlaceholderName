ae77ca309536d933e3bbba4629808e1acf640c8a
Federico Perez <fedelll@gmail.com>
2020-04-09 16:43:28 +0200

    Recipe: 2 wheats = 1 axe

--- Assets/Scripts/Inventory/CraftRecipeDatabase.cs
+++ Assets/Scripts/Inventory/CraftRecipeDatabase.cs
@@ -29,22 +29,10 @@ public class CraftRecipeDatabase : MonoBehaviour {
     {
         recipes = new List<CraftRecipe>()
         {
-            new CraftRecipe(1,
-            new int[] {
-                0, 0, 0,
-                0, 2, 0,
-                0, 0, 0
-            }),
             new CraftRecipe(2,
             new int[] {
-                3, 0, 0,
-                0, 3, 0,
-                0, 0, 3
-            }),
-            new CraftRecipe(3,
-            new int[] {
-                1, 0, 2,
-                0, 2, 0,
+                1, 1, 0,
+                0, 0, 0,
                 0, 0, 0
             })
         };
